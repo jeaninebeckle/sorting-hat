16dcba59c8fd2240b9606dcd2142a9eea177ec7f
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-04-30 20:32:59 -0500

    randomly sorts students into houses and pushes to array. button needs fixing

--- main.js
+++ main.js
@@ -5,38 +5,57 @@ const showFirstForm = () => {
   }
 }
 
-const validateForm = () => {
-  let x = document.forms["myForm"]["fName"].value;
-  if (x == "" || x == null) {
-    alert ("Even though the Sorting Hat can read minds, you must type a name in the box first!");
-    return false;
-  }
+// const validateForm = () => {
+//   let x = document.forms["myForm"]["fName"].value;
+//   if (x == "" || x == null) {
+//     alert ("Even though the Sorting Hat can read minds, you must type a name in the box first!");
+//     return false;
+//   }
+// }
+
+const houses =["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
+const sortedStudents = []
+
+function assignToHouse() {
+  let randomNumber = Math.floor(Math.random() * (houses.length));
+  document.getElementById("startSort").value= houses[randomNumber];
+  sortedStudents.push(houses[randomNumber])
+  console.log(sortedStudents)
 }
-
-
-
-
+// const studentArray = [];
 
 // const printToDom = (selector, textToPrint) => {
 //   const selectedDiv = document.querySelector(selector);
 //   selectedDiv.innerHTML = textToPrint;
 // }
 
-// const buildSortForm = () => {
-//   let domString = '';
+// const buildStudentCards = (studentArray) => {
+//   let domString = '<div class="row">';
+
+//   domString +=
+//     <div class="col-sm-6">
+//       <div class="card">
+//         <div class="card-body">
+//           <h5 class="card-title">Student Name</h5>
+//           <p class="card-text">House</p>
+//           <a href="#" class="btn btn-primary">Expel</a>
+//         </div>
+//       </div>
+//     </div>
 // }
 
 
 
-// const showSortForm = (event) => {
-//   const buttonId = event.target.id;
-// }
+// // const buildStudentCards = (event) => {
+// //   const buttonId = event.target.id;
+// // }
 
 // const clickEvents = () => {
-//   document.querySelector("#sortButton").addEventListener("click", showSortForm)
+//   document.querySelector("#startSort").addEventListener("click", assignToHouse)
 // }
 
 // const init = () => {
+//   // buildStudentCards(studentArray)
 //   clickEvents();
 // }
 
