74b16b6f2e7368eb8f73f033be5eae1269b865e9
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-05-02 11:01:45 -0500

    pushing code so Matt can look at it

--- main.js
+++ main.js
@@ -24,32 +24,33 @@ const assignToHouse = () => {
 }
 
 
+let studentArray = [];
 
-  let studentArray = [];
+const clickSortButton = () => {
+  validateForm();
+  assignToHouse();
+  let newStudent = {};
+  newStudent['name'] = document.getElementById("inputPassword2").value;
+  newStudent['house'] = houses[Math.floor(Math.random() * (houses.length))];
+  newStudent['uniqueId'] = Date.now();
+  console.log(newStudent);
+  studentArray.push(newStudent);
+  console.log(studentArray)
+  buildStudentCards(studentArray);
+  document.getElementById("inputPassword2").value = null;
+  }
 
-  const clickSortButton = () => {
-    //clicking the Sort! button needs to take the input text from the HTML form and merge it with the randomly generated house.
-   //then it needs to push an object of student name and house to an empty array
-   //then the buildStudentCards function needs to pull the info from that array and print to DOM
-   let newStudent = {};
-   newStudent['name'] = document.getElementById("inputPassword2").value;
-   newStudent['house'] = houses[Math.floor(Math.random() * (houses.length))];
-   newStudent['uniqueid'] = Date.now();
-   console.log(newStudent);
-   studentArray.push(newStudent);
-   console.log(studentArray)
-   buildStudentCards(studentArray);
-}
 
-const printToDom = (selector, textToPrint) => {
-  const selectedDiv = document.querySelector(selector);
+
+const printToDom = (divId, textToPrint) => {
+  const selectedDiv = document.getElementById(divId);
   selectedDiv.innerHTML = textToPrint;
 }
 
 const buildStudentCards = (studentArray) => {
   let domString = '<div class="row">';
 
-  if (document.getElementById("inputPassword2").value !== "" || null) {
+   if (document.getElementById("inputPassword2").value !== "" || null) {
 
     for (let i=0; i<studentArray.length; i++) {
 
@@ -59,30 +60,46 @@ const buildStudentCards = (studentArray) => {
           <div class="card-body">
             <h5 class="card-title">${studentArray[i].name}</h5>
             <p class="card-text">${studentArray[i].house}</p>
-            <a href="#" class="btn btn-primary">Expel</a>
+            <button id="expel" class="btn btn-primary">Expel</button>
           </div>
         </div>
       </div>
       `;
+      console.log(studentArray[i].uniqueId)
   }
   domString += '</div>'
 
-  printToDom("#students", domString)
+  printToDom("students", domString)
   }
 }
 
+const expelStudent = () => {
+  let idValue = '';
+  studentArray.forEach(student => {
+     if (studentArray.uniqueId === id) {
+      console.log(student.uniqueId)
+      idValue = student.indexOf(student);
+     }
+  });
+  console.log(idValue)
+  return idValue;
+}
+
+const findStudents = () => {
+  expelStudent(studentArray[0].uniqueId)
+}
+
+let voldemortsArmy = []
 
 const clickEvents = () => {
-  document.querySelector("#startSort").addEventListener("click", validateForm);
-  document.querySelector("#startSort").addEventListener("click", assignToHouse);
-  document.querySelector("#startSort").addEventListener("click", clickSortButton);
-  
+  document.getElementById("startSort").addEventListener("click", clickSortButton);
+  console.log("Made it past startsort buttons")
+  document.getElementById("expel").addEventListener("click", findStudents);
  }
 
 const init = () => {
+  buildStudentCards(studentArray)
   clickEvents();
-  // buildStudentCards(studentArray)
-  
 }
 
 init ();
